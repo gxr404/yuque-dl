30a87ebf15dc8d159fca2b966d3a7ae2cb2bc01e
gxr404 <1650542259@qq.com>
2024-09-11 09:05:22 +0800

    fix: 优化nodejs版本过低提示

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
 
 function run() {
-  return import('../dist/es/cli.js')
+  return import('../dist/es/cli.js').catch((e) => {
+    const lowVersionError = [
+      'Not supported',
+      'No such built-in module: node:fs/promises',
+      'Only file and data URLs are supported by the default ESM loader',
+      // at Loader.moduleStrategy (internal/modules/esm/translators.js:145:18)
+      'Unexpected token \'??=\''
+    ]
+    if (lowVersionError.includes(e.message)) {
+      console.error('\x1b[31m%s\x1b[0m', '✕ nodejs版本过低')
+      return
+    }
+    throw e
+  })
 }
 run()
